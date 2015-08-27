efd900fd7a0422b15b1359a4c4aee6934c37233c
unknown <mcherry@USMAYMCHERRYPC2.ra-int.com>
2015-08-27 17:11:37 -0400

    2 new sites added, rows working with ng-class-even!

--- js/cherry-portfolio.js
+++ js/cherry-portfolio.js
@@ -5,5 +5,7 @@ angular.module('cherry-portfolio', [])
 			{img: 'images\\resume.png', text: 'Resume', link: 'http://rawgit.com/mttchrry/frontend-nanodegree-resume/master/index.html'},
 			{img: 'images\\froggerGame.PNG', text: 'Frogger Arcade Game', link: 'http://rawgit.com/mttchrry/frontend-nanodegree-arcade-game/master/index.html'},
 			{img: 'images\\cleveland-skyline.jpg', text: 'Cleveland Hotspots', link: 'http://mttchrry.github.io/MapProject5/'},
-			{img: 'images\\Ds3.png', text: 'Shortest Path Visualizer', link: 'http://mttchrry.github.io/CrossChxTravellingSalesman/'}];
+			{img: 'images\\Ds3.png', text: 'Shortest Path Visualizer', link: 'http://mttchrry.github.io/CrossChxTravellingSalesman/'},
+			{img: 'images\\game-2048-java.png', text: 'My 2048 Mods', link: 'http://mttchrry.github.io/2048/'},
+			{img: 'images\\moving.jpg', text: 'Move Planner', link: 'http://mttchrry.github.io/FEDLesson4MovePlanner'}];
 	});
