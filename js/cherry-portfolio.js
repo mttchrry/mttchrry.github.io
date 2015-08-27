4ba7b30254039a6ba7f5a3291b659dade3fd04f4
unknown <mcherry@USMAYMCHERRYPC2.ra-int.com>
2015-08-27 16:12:49 -0400

    renaming an index file

--- js/cherry-portfolio.js
+++ js/cherry-portfolio.js
@@ -4,5 +4,6 @@ angular.module('cherry-portfolio', [])
 		accomplishments.list = [
 			{img: 'images\\resume.png', text: 'Resume', link: 'http://rawgit.com/mttchrry/frontend-nanodegree-resume/master/index.html'},
 			{img: 'images\\froggerGame.PNG', text: 'Frogger Arcade Game', link: 'http://rawgit.com/mttchrry/frontend-nanodegree-arcade-game/master/index.html'},
-			{img: 'images\\cleveland-skyline.jpg', text: 'Cleveland Hotspots', link: 'http://mttchrry.github.io/MapProject5/'}];
+			{img: 'images\\cleveland-skyline.jpg', text: 'Cleveland Hotspots', link: 'http://mttchrry.github.io/MapProject5/'},
+			{img: 'images\\Ds3.png', text: 'Shortest Path Visualizer', link: 'http://mttchrry.github.io/CrossChxTravellingSalesman/'}];
 	});
