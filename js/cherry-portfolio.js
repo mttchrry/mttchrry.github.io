b6d45f3216d8a377e73f5cea19468d28d0c84be6
unknown <mcherry@USMAYMCHERRYPC2.ra-int.com>
2015-08-27 16:03:36 -0400

    Adding Angular JS Goodness

---
+++ js/cherry-portfolio.js
@@ -0,0 +1,8 @@
+angular.module('cherry-portfolio', [])
+	.controller('AccomplishmentsController', function() {
+		var accomplishments = this;
+		accomplishments.list = [
+			{img: 'images\\resume.png', text: 'Resume', link: 'http://rawgit.com/mttchrry/frontend-nanodegree-resume/master/index.html'},
+			{img: 'images\\froggerGame.PNG', text: 'Frogger Arcade Game', link: 'http://rawgit.com/mttchrry/frontend-nanodegree-arcade-game/master/index.html'},
+			{img: 'images\\cleveland-skyline.jpg', text: 'Cleveland Hotspots', link: 'http://mttchrry.github.io/MapProject5/'}];
+	});
