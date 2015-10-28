ed14d73bf0c35566c47b0f8c0d7cb95a3fcecc01
unknown <mcherry@USMAYMCHERRYPC2.ra-int.com>
2015-10-28 16:10:42 -0400

    adding background colors and blog link

--- js/cherry-portfolio.js
+++ js/cherry-portfolio.js
@@ -3,6 +3,7 @@ angular.module('cherry-portfolio', [])
 		var accomplishments = this;
 		accomplishments.list = [
 			{img: 'images\\resume.png', text: 'Resume', link: 'http://rawgit.com/mttchrry/frontend-nanodegree-resume/master/index.html', summary: 'This is My Resume, its got a list of a lot of my accomplishments and work and through formal education. Check it out on the link!'},
+			{img: 'images\\blogspot.png', text: "Cherry Blog", link: 'http://mrcherryontop.appspot.com/blog', summary: 'A little editable blog I put together with Python and google app engine'},
 			{img: 'images\\froggerGame.PNG', text: 'Frogger Arcade Game', link: 'http://rawgit.com/mttchrry/frontend-nanodegree-arcade-game/master/index.html'},
 			{img: 'images\\cleveland-skyline.jpg', text: 'Cleveland Hotspots', link: 'http://mttchrry.github.io/MapProject5/'},
 			{img: 'images\\Ds3.png', text: 'Shortest Path Visualizer', link: 'http://mttchrry.github.io/CrossChxTravellingSalesman/', summary: 'pretty cool graphics eh? And Djikstras algorithm? whats not to like?'},
